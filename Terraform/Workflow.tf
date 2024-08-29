5af0f6abe0f6ae22b68374a257910d47d5eee587
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-08-29 10:08:49 -0400

    Update Workflow.tf

--- Terraform/Workflow.tf
+++ Terraform/Workflow.tf
@@ -1,2 +1,8 @@
-# Terraform Initialization
+# Ensure that you're authenticated
+source .env
+
+# Initialize Terraform in the directory where your Terrform configuration file is
 terraform init 
+
+# 
+terraform plan
