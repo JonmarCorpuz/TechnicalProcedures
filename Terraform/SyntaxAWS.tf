9ba3054b287bd2cb9042c32333e63b373a4bef69
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-08-28 20:39:23 -0400

    Update SyntaxAWS.tf

--- Terraform/SyntaxAWS.tf
+++ Terraform/SyntaxAWS.tf
@@ -7,6 +7,10 @@ terraform {
   }
 }
 
+provide "aws" {
+  region = "<aws_region>"
+}
+
 // VPC
 resource "aws_vpc" "<resource_name>" {
   cidr_block = "<ipv4_address>/<prefix_length>"
@@ -22,3 +26,19 @@ resource "aws_subnet" "<resource_name>" {
 resource "aws_internet_gateway" "<resource_name>" {
   vpc_id = aws_vpc.<vpc_id>
 }
+
+// Route Table
+resource "aws_route_table" "<resource_name>" {
+  vpc_id = aws_vpc.<vpc_id>
+
+  route {
+    cidr_block = "<ipv4_address>/<prefix_length>" // Addresses to route
+    gateway_id = aws_internet_gateway.<resource_name>.id
+  }
+}
+
+// Associate a route table to a subnet
+resource "aws_route_table_association" "<resource_name>" {
+  subnet_id = aws_subnet.<subnet_resource_name>.id
+  route_table_id = aws_route_table.<route_table_resource_name>.id
+} 
