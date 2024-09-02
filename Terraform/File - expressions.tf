03091f9c37a13d41f4c5403dc394a99a085c3d26
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-09-02 15:26:06 -0400

    Update File - expressions.tf

--- Terraform/File - expressions.tf
+++ Terraform/File - expressions.tf
@@ -1,8 +1,8 @@
 # Produce a tuple
-[for x in var.<variable_id> : <actions>]
+[for <key> in var.<variable_id> : <actions>]
 
 # Produce an object
-{for x in var.<variable_id> : <actions>}
+{for <key> in var.<variable_id> : <actions>}
 
 # Map or transform elements from a collection into a new form
-{for x in var.<variable_id> : x => <actions>}
+{for <key> in var.<variable_id> : <key> => <value>}
