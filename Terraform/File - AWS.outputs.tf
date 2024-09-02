a1896e184ee1a364e6f2ce7066b4be604ac10627
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-09-02 14:31:58 -0400

    Update File - AWS.outputs.tf

--- Terraform/File - AWS.outputs.tf
+++ Terraform/File - AWS.outputs.tf
@@ -2,10 +2,12 @@
 output "<output_block_id>" {
   description = "<output_description>"
   value       = aws_s3_bucket.<s3_bucket_resource_id>.bucket
+  sensitive   = {true|false}
 }
 
 #
 output "<output_block_id>" {
   description = "<output_description>"
   value       = aws_s3_bucket.<s3_bucket_resource_id>.website_endpoint
+  sensitive   = {true|false}
 }
