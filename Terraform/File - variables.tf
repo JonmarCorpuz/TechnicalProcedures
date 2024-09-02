f020c15968a35c56988490379845d38c21985f48
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-09-02 09:03:08 -0400

    Update File - variables.tf

--- Terraform/File - variables.tf
+++ Terraform/File - variables.tf
@@ -1,3 +1,10 @@
-variable "<variable_name>" {
-  type = <variable_type>
+variable "<variable_id>" {
+  type        = <variable_type>
+  default     = <default_value>
+  description = "<description>"
+
+  validation {
+    condition     = var.<variable_id> == "<value>" [|| var.<variable_id> == "<value>"] || contains(["<value1>", "<value2>, ...], var.<variable_id>)
+    error_message = "<error_message>"
+  }
 }
