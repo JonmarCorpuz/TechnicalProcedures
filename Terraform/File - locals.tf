f63ca83663b414ac00a4a5c22c674eaad161716b
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-09-02 14:36:02 -0400

    Update File - locals.tf

--- Terraform/File - locals.tf
+++ Terraform/File - locals.tf
@@ -4,15 +4,16 @@ locals {
   project_owner = "<project_id>"
   cost_center   = "<cost_center>"
   managed_by    = "Terraform"
+  sensitive_tag = {true|false}
 }
 
 # Local Object Variables
 locals {
   <local_object_name> = {
-    Name       = "<value>"
-    Env        = "<value>"
-    ManagedBy  = "<value>"
-    Project    = "<value>"
-    CostCenter = "<value>"
+    project       = "<project_folder_name>"
+    project_owner = "<project_id>"
+    cost_center   = "<cost_center>"
+    managed_by    = "Terraform"
+    sensitive_tag = {true|false}
   }
 }
