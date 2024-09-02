5629d765f1af7b48f49be31623b227dd30efa9b1
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-09-01 20:05:23 -0400

    Update AWS - Data Sources.tf

--- Terraform/AWS - Data Sources.tf
+++ Terraform/AWS - Data Sources.tf
@@ -1,5 +1,5 @@
 # External AMI
-data "aws_ami" "<resource_id>" {
+data "aws_ami" "<data_source_id>" {
   most_recent = {true|false}
   owner       = ["<owner_id>"]
   provider    = <provider>
@@ -13,4 +13,31 @@ data "aws_ami" "<resource_id>" {
     name   = "virtualization_type"
     values = ["<virtualization_type>"]
   }
+}
+
+# 
+data "aws_caller_identity" "<data_source_id>" {
+  
+}
+
+#
+data "aws_region" "<data_source_id>" {
+  provider = aws.<provider_resource_id>
+}
+
+# 
+data "aws_vpc" "<data_source_id>" {
+  tags = {
+    Env = "<environment_tag>"
+  }
+}
+
+#
+data "aws_availability_zones" "<data_source_id>" {
+  state = "available"
+}
+
+# 
+data "aws_iam_policy_document" "<data_source_id>" {
+
 }
