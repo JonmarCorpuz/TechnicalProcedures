396e8c5de5f75dfafb79021c999dd35fffaf17b5
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-10-24 19:12:31 -0400

    Delete Terraform/File - expressions.tf

--- Terraform/File - expressions.tf
+++
@@ -1,8 +0,0 @@
-# Produce a tuple
-[for <key> in var.<variable_id> : <actions>]
-
-# Produce an object
-{for <key> in var.<variable_id> : <actions>}
-
-# Map or transform elements from a collection into a new form
-{for <key> in var.<variable_id> : <key> => <new_value> [conditions]}
